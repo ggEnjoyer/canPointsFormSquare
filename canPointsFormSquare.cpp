430a1f9f797878b143218a52898411989d1c255d
Илья К <watttt311@gmail.com>
2021-10-06 14:32:16 +0300

    Now it can decide whether these point can form a square or not. Does not work of any of sides is parallel to any 2d axis

--- canPointsFormSquare.cpp
+++ canPointsFormSquare.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <conio.h>
+#include <math.h>
 
 using namespace std;
 
@@ -12,12 +13,20 @@ struct Point
 
 bool isSquare(Point p1, Point p2, Point p3, Point p4 )
 {
+    double
+        AB,
+        BC,
+        CD,
+        DA,
+        AC,
+        BD;
     int
         xtop,
         xbot,
-        xalmosttop,
-        xalmostbot,
+        ytop,
+        ybot,
         i,
+        id,
         xes[] = { p1.x, p2.x, p3.x, p4.x },
         yes[] = { p1.y, p2.y, p3.y, p4.y };
     Point
@@ -26,27 +35,126 @@ bool isSquare(Point p1, Point p2, Point p3, Point p4 )
         C,
         D;
 
-    if ((p1.x == p2.x && p1.x == p3.x) || (p4.x == p2.x && p4.x == p3.x) || (p1.y == p2.y && p1.y == p3.y) || (p4.y == p2.y && p4.y == p3.y)
-        || (p1.x == p2.x && p1.y == p2.y) || (p2.x == p3.x && p2.y == p3.y) || (p3.x == p4.x && p3.y == p4.y) || (p1.x == p4.x && p1.y == p4.y)) return false;
-
+    //if ((p1.x == p2.x && p1.y == p2.y) || (p2.x == p3.x && p2.y == p3.y) || (p3.x == p4.x && p3.y == p4.y) || (p1.x == p4.x && p1.y == p4.y)) return false;
+    //define xtop
     i = 1;
-    xtop = p1.x;
-    while (i < 3)
+    id = 0;
+    xtop = xes[0];
+    while (i < 4)
     {
-        if (xtop < xes[i])xtop = xes[i];
+        if (xtop < xes[i]) 
+        {
+            xtop = xes[i]; 
+            id = i;
+        }
         i++;
     }
+    A.x = xtop; 
+    A.y = yes[id];
+    cout << endl << A.x << " " << A.y;
 
+    //define xbot
     i = 1;
-    xbot = p1.x;
+    id = 0;
+    xbot = xes[0];
     while (i < 3)
     {
-        if (xtop < xes[i])xtop = xes[i];
+        if (xbot > xes[i])
+        {
+            xbot = xes[i];
+            id = i;
+        }
         i++;
     }
+    B.x = xbot;
+    B.y = yes[id];
+    cout << endl << B.x << " " << B.y;
+
+    //define ytop
+    i = 1;
+    id = 0;
+    ytop = yes[0];
+    while (i < 4)
+    {
+        if (ytop < yes[i])
+        {
+            ytop = yes[i];
+            id = i;
+        }
+        i++;
+    }
+    C.y = ytop;
+    C.x = xes[id];
+    cout << endl << C.x << " " << C.y;
+
+    //define ybot
+    i = 1;
+    id = 0;
+    ybot = yes[0];
+    while (i < 3)
+    {
+        if (ybot > yes[i])
+        {
+            cout << endl << ybot << " > " << yes[i];
+            ybot = yes[i];
+            id = i;
+        }
+        i++;
+    }
+    D.y = ybot;
+    D.x = xes[id];
+    cout << endl << D.x << " " << D.y;
+
+    AC = sqrt(pow((A.x - C.x), 2) + pow((A.y - C.y), 2));
+    cout << endl << AC;
+    BC = sqrt(pow((B.x - C.x), 2) + pow((B.y - C.y), 2));
+    cout << endl << BC;
+    BD = sqrt(pow((B.x - D.x), 2) + pow((B.y - D.y), 2));
+    cout << endl << BD;
+    DA = sqrt(pow((D.x - A.x), 2) + pow((D.y - A.y), 2));
+    cout << endl << DA;
+
+    AB = sqrt(pow((A.x - B.x), 2) + pow((A.y - B.y), 2));
+    cout << endl << AB;
+    CD = sqrt(pow((C.x - D.x), 2) + pow((C.y - D.y), 2));
+    cout << endl << CD;
+    
+   // if ((AB == CD) && (AC == CD) && (BC == BD) && (DA == AC) && (AC == DA) && (AC != 0)) 
+        if (AB == CD)
+        {
+            cout << "\nab = cd...";
+            if (AC == BC)
+            {
+                cout << "\nac = cd...";
+                if (BC == BD)
+                {
+                    cout << "\nbc = bd...";
+                    if (DA == AC)
+                    {
+                        cout << "\nda = ac...";
+                        if (AC == DA)
+                        {
+                            cout << "\nac = da...";
+                            if (AC != 0)
+                            {
+                                cout << "\nac is not 0...\nso it`s true!";
+                                return 1;
+                            }
+                            cout << "\nbut ac is 0, so it is false.";
+                        }
+                        cout << "\n but ac != da";
+                    }
+                    cout << "\nbut da != ac";
+                }
+                cout << "\nbut bc != bd";
+            }
+            cout << "\nbut ac != bc";
+        }
+        cout << "\nab != cd";
+    return 0;
 }
 
-bool Sort(int i[])
+void Sort(int i[], int &f)
 {
     int j = 1, net;
     bool b = false;
@@ -70,10 +178,11 @@ bool Sort(int i[])
 
 int main()
 {
+    int i[4];
     Point p1, p2, p3, p4;
     while (_getch() != 27)
     {
-        cout << "\n--------------------------------------------------";
+        cout << "\n--------------------------------------------------\n";
         cout << "\nEnter X and Y coordinates of point 1: ";
         cin >> p1.x >> p1.y;
         cout << "\nEnter X and Y coordinates of point 2: ";
@@ -82,7 +191,7 @@ int main()
         cin >> p3.x >> p3.y;
         cout << "\nEnter X and Y coordinates of point 4: ";
         cin >> p4.x >> p4.y;
-        if (isSquare(p1, p2, p3, p4) == 0)cout << "They totally cant make square";
+        if (isSquare(p1, p2, p3, p4) == 0)cout << "\nThey totally cant make square";
         else cout << "\nThere is a possibility that these points can make square";
     }
 }
